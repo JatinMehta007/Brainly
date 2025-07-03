a52f584db5d2074f3afeba367947a78fb4c9fdfe
Jatin_Mehta <mehtajatin045@gmail.com>
2025-07-03 09:26:11 +0530

    add card

--- brainly-frontend/src/App.tsx
+++ brainly-frontend/src/App.tsx
@@ -1,20 +1,36 @@
 // import { useState } from 'react'
+import { useState } from 'react'
 import './App.css'
 import { Button } from './components/ui/Button'
+import { Card } from './components/ui/Card'
+import { CreateContentModal } from './components/ui/CreateContentModel'
 import { PlusIcon } from './icons/PlusIcon'
 // import { Buttons } from './components/ui/Buttons'
 import { ShareIcon } from './icons/ShareIcon'
+import { SideBar } from './components/ui/Sidebar'
 
 function App() {
-
+  const [modalOpen, setModalOpen] = useState(false);
   return (
-    <>
-     
-     <Button startIcon={<PlusIcon size='md'></PlusIcon>}variant="primary" text="Add Content " size='md' ></Button>
+  <div>
+    <SideBar/>
+     <div className='p-4 ml-72 min-h-screen bg-gray-100 border-2'>
+      <CreateContentModal open={modalOpen} onClose={()=>{
+        setModalOpen(false)
+      }} />
+      <div className='flex justify-end gap-4'>
+     <Button onClick={()=>{
+       setModalOpen(true)
+      }} startIcon={<PlusIcon size='md'></PlusIcon>}variant="primary" text="Add Content " size='md' ></Button>
      <Button   startIcon={<ShareIcon size='md'></ShareIcon>}  variant="secondary" text="Share Brain" size='md'></Button>
-     
-     {/* <Buttons EndIcon={<ShareIcon size='lg'></ShareIcon>} StartIcon={<PlusIcon size='lg'></PlusIcon>} size='lg' text='hero honda' variant='primary' ></Buttons> */}
-    </>
+     </div>
+     <div className='flex gap-4'>
+     <Card title='twitter' type='twitter' link='https://twitter.com/JatinMehta35630/status/1940102573698245061'></Card>
+      <Card title='FIRST video' type='youtube' link='https://www.youtube.com/watch?v=0itY1Fhvnnk'></Card>
+      </div>
+     </div>
+      </div>
+    
   )
 }
 
