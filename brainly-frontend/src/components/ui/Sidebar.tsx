091a9c35fddfb81b38fd7adf789e75d1a5656fc2
Jatin_Mehta <mehtajatin045@gmail.com>
2025-07-03 09:28:02 +0530

    add sidebarItem

---
+++ brainly-frontend/src/components/ui/Sidebar.tsx
@@ -0,0 +1,20 @@
+import { ShareIcon } from "../../icons/ShareIcon";
+import { TwitterIcon } from "../../icons/TwitterIcon";
+import { YoutubeIcon } from "../../icons/YoutubeIcon";
+import { SideBarItem } from "./SidebarItem";
+
+export function SideBar(){
+    return <div className="h-screen bg-white border-r w-72  fixed left-0 top-0 pl-6">
+            <div className="pl-4 flex text-2xl pt-8 items-center">
+                <div className="pr-2 text-purple-400"> 
+                    <ShareIcon size="lg"/>
+                </div>
+                Brainly
+            </div>
+        <div className="pt-8 pl-4  ">
+            <SideBarItem text="Twitter" icon={<TwitterIcon></TwitterIcon>}/>
+
+            <SideBarItem text="Youtube" icon={<YoutubeIcon></YoutubeIcon>}/>
+        </div>
+    </div>
+}
