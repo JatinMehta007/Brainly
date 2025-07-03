bc64e26f1146b1bcf9f747174a159da0e56c13a6
Jatin_Mehta <mehtajatin045@gmail.com>
2025-07-03 09:26:50 +0530

    add defaultstyles

--- brainly-frontend/src/components/ui/Button.tsx
+++ brainly-frontend/src/components/ui/Button.tsx
@@ -14,15 +14,15 @@ const VariantStyles= {
     "secondary" : "bg-purple-300 text-purple-600"
 }
 
-const sizeStyles ={
-    "sm" : "px-2 py-1",
-    "md" : "px-4 py-2",
-    "lg" : "px-6 py-4 "
-}
+// const sizeStyles ={
+//     "sm" : "px-2 py-1",
+//     "md" : "px-4 py-2",
+//     "lg" : "px-6 py-4 "
+// }
 const defaultStyles = "rounded-md  px-4 py-2 flex justify-center items-center ";
 
-export const Button=({variant, text, startIcon, endIcon} : ButtonProps)=>{
-    return  <button className={`${VariantStyles[variant]} + ${defaultStyles}`} >
+export const Button=({variant, text, startIcon, endIcon,onClick} : ButtonProps)=>{
+    return  <button onClick={onClick} className={`${VariantStyles[variant]} + ${defaultStyles}`} >
          <div className="flex items-center ">
          {startIcon ? <div className="pr-2">{startIcon}</div> : null} {text} {endIcon} 
          </div>
