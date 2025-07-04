2a0858ddae1c35cbed73ec80d5e10d5c805c27e2
Jatin_Mehta <mehtajatin045@gmail.com>
2025-07-04 09:25:08 +0530

    add createModel

--- brainly-frontend/src/components/ui/CreateContentModel.tsx
+++ brainly-frontend/src/components/ui/CreateContentModel.tsx
@@ -1,32 +1,89 @@
+import { useRef, useState } from "react";
 import { CrossIcon } from "../../icons/CrossIcon";
 import { Button } from "./Button";
+import { Input } from "./Input";
+import axios from "axios";
+import { BACKEND_URL } from "../../config";
 
-export function CreateContentModal({open , onClose}){
-    return <div>
-        {open && <div className="w-screen h-screen bg-slate-500 fixed top-0 left-0 opacity-60 flex justify-center items-center">
-            <div className="bg-white opacity-100 p-4 rounded ">
-             <div className="flex justify-end ">
-                <div onClick={onClose} className="cursor-pointer">
-                <CrossIcon/>
-                </div>
-             </div>
-             <div>
-                <Input placeholder={"Title"}></Input>
-                <Input placeholder={"Link"}></Input>
-             </div>
-             <div className="flex justify-center ">
-             <Button variant="primary" text="Submit"></Button>
-             </div>
-            </div>
-
-        </div> }
-
-    </div>
-
+enum ContentType {
+  Youtube = "youtube",
+  Twitter = "twitter",
 }
 
-function Input({onChange, placeholder} : {onChange : () => void}){
-    return <div>
-        <input placeholder={placeholder} type="text" className=" px-4 py-2 border rounded m-2" onChange={onChange} />
+export function CreateContentModal({ open, onClose }) {
+  const titlRef = useRef<HTMLInputElement>();
+  const linkRef = useRef<HTMLInputElement>();
+  const [type, setType] = useState(ContentType.Youtube);
+
+  async function addContent() {
+    const title = titlRef.current?.value;
+    const link = linkRef.current?.value;
+
+    await axios.post(
+      `${BACKEND_URL}/api/v1/content`,
+      {
+        link,
+        title,
+        type,
+      },
+      {
+        headers: {
+          "Authorization": localStorage.getItem("token"),
+        },
+      }
+    );
+    onClose(); 
+  }
+  return (
+    <div>
+      {open && (
+        <div>
+          <div className="w-screen h-screen bg-slate-500 fixed top-0 left-0 opacity-60 flex justify-center items-center"></div>
+          <div className="w-screen h-screen  fixed top-0 left-0 opacity-100 flex justify-center items-center">
+            <div className="bg-white  p-4 rounded  ">
+              <div className="flex justify-end ">
+                <div onClick={onClose} className="cursor-pointer">
+                  <CrossIcon />
+                </div>
+              </div>
+              <div>
+                <Input reference={titlRef} placeholder={"Title"}></Input>
+                <Input reference={linkRef} placeholder={"Link"}></Input>
+              </div>
+              <div>
+                <h1>Type</h1>
+                <div className="flex gap-1 p-4 justify-center">
+                  <Button
+                    text="Youtube"
+                    variant={
+                      type === ContentType.Youtube ? "primary" : "secondary"
+                    }
+                    onClick={() => {
+                      setType(ContentType.Youtube);
+                    }}
+                  ></Button>
+                  <Button
+                    text="Twitter"
+                    variant={
+                      type === ContentType.Twitter ? "primary" : "secondary"
+                    }
+                    onClick={() => {
+                      setType(ContentType.Twitter);
+                    }}
+                  ></Button>
+                </div>
+              </div>
+              <div className="flex justify-center ">
+                <Button
+                  onClick={addContent}
+                  variant="primary"
+                  text="Submit"
+                ></Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
+  );
 }
