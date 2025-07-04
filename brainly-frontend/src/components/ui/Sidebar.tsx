2a5292e6a482e33ff663ec042f270007f02fa95b
Jatin_Mehta <mehtajatin045@gmail.com>
2025-07-04 09:26:19 +0530

    add sidebar

--- brainly-frontend/src/components/ui/Sidebar.tsx
+++ brainly-frontend/src/components/ui/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { ShareIcon } from "../../icons/ShareIcon";
+
 import { TwitterIcon } from "../../icons/TwitterIcon";
 import { YoutubeIcon } from "../../icons/YoutubeIcon";
 import { SideBarItem } from "./SidebarItem";
@@ -6,8 +6,8 @@ import { SideBarItem } from "./SidebarItem";
 export function SideBar(){
     return <div className="h-screen bg-white border-r w-72  fixed left-0 top-0 pl-6">
             <div className="pl-4 flex text-2xl pt-8 items-center">
-                <div className="pr-2 text-purple-400"> 
-                    <ShareIcon size="lg"/>
+                <div className="pr-2 "> 
+                    <img src="./Logo.png" alt="" className="h-9 w-9" />
                 </div>
                 Brainly
             </div>
