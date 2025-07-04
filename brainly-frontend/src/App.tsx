9c17e54ab3bf10bbb6443f7d325d6fd1d6c8f43e
Jatin_Mehta <mehtajatin045@gmail.com>
2025-07-04 09:22:30 +0530

    add sigup route

--- brainly-frontend/src/App.tsx
+++ brainly-frontend/src/App.tsx
@@ -1,37 +1,18 @@
-// import { useState } from 'react'
-import { useState } from 'react'
-import './App.css'
-import { Button } from './components/ui/Button'
-import { Card } from './components/ui/Card'
-import { CreateContentModal } from './components/ui/CreateContentModel'
-import { PlusIcon } from './icons/PlusIcon'
-// import { Buttons } from './components/ui/Buttons'
-import { ShareIcon } from './icons/ShareIcon'
-import { SideBar } from './components/ui/Sidebar'
+import { Dashboard } from "./pages/dashboard"
+import { Signin } from "./pages/Signin"
+import { Signup } from "./pages/Signup"
+import { BrowserRouter, Routes , Route } from "react-router-dom"
 
 function App() {
-  const [modalOpen, setModalOpen] = useState(false);
-  return (
-  <div>
-    <SideBar/>
-     <div className='p-4 ml-72 min-h-screen bg-gray-100 border-2'>
-      <CreateContentModal open={modalOpen} onClose={()=>{
-        setModalOpen(false)
-      }} />
-      <div className='flex justify-end gap-4'>
-     <Button onClick={()=>{
-       setModalOpen(true)
-      }} startIcon={<PlusIcon size='md'></PlusIcon>}variant="primary" text="Add Content " size='md' ></Button>
-     <Button   startIcon={<ShareIcon size='md'></ShareIcon>}  variant="secondary" text="Share Brain" size='md'></Button>
-     </div>
-     <div className='flex gap-4'>
-     <Card title='twitter' type='twitter' link='https://twitter.com/JatinMehta35630/status/1940102573698245061'></Card>
-      <Card title='FIRST video' type='youtube' link='https://www.youtube.com/watch?v=0itY1Fhvnnk'></Card>
-      </div>
-     </div>
-      </div>
-    
-  )
+  return<BrowserRouter>
+      <Routes>
+
+        <Route path="/" element={<Signup/>}></Route>
+        <Route path="/signin" element={<Signin/>}></Route>
+        <Route path="/dashboard" element={<Dashboard/>}></Route>
+      </Routes>
+  </BrowserRouter>
+  
 }
 
 export default App
